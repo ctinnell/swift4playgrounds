33bbf984bb0fdc18799e56d07a104715d1847ca5
Clay Tinnell <ctinnell@gaig.com>
2017-09-19 05:37:42 -0400

    added json encoding playground

---
+++ jsonEncoding.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Language: Codable {
+    var name: String
+    var version: Int
+}
+
+let swift = Language(name: "Swift", version: 4)
+let php = Language(name: "PHP", version: 7)
+let perl = Language(name: "Perl", version: 6)
+
+let encoder = JSONEncoder()
+
+if let encoded = try? encoder.encode(swift) {
+    if let json = String(data: encoded, encoding: .utf8) {
+        print(json);
+    }
+    
+    let decoder = JSONDecoder()
+    
+    if let decoded = try? decoder.decode(Language.self, from: encoded) {
+        print(decoded.name)
+    }
+}
